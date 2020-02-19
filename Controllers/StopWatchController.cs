99eea42b791fe923963ac2f46866904bc25ead48
Arpit Malik <Arpit.Malik@asos.com>
2020-02-19 17:04:38 +0530

    First commit

---
+++ Controllers/StopWatchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+    
+
+namespace StopWatch.Controllers
+{
+    public class StopWatchController : Controller
+    {
+        private string serviceBusConnectionString = "Endpoint=sb://stopwatchmessanger.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9ZnB0nAufUN6hIcFvWLIcT6gesobxSYTfTohEkmz/ZU=";
+        private string queueName = "stopwatchqueue";
+        public IActionResult Duration()
+        {
+            return View();
+        }
+
+        public void SendToASB(string inputData)
+        {
+            var queueClient = new QueueClient(serviceBusConnectionString, queueName);
+            var messageBody = $"Message From Web";
+            var message = new Message(Encoding.UTF8.GetBytes(inputData));
+            queueClient.SendAsync(message);
+        }
+    }
+}
